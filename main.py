fc9d744ad7a7bc424aec7c2e80206dbeaa1889d2
rothman5 <rothman@ualberta.ca>
2022-08-07 00:13:04 -0600

    first command

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+# Loads the .env file
+load_dotenv()
+
+# Get the API token from the .env file
+tk = os.getenv("TOKEN")
+
+# Create a client object from the discord.py module
+bot = discord.Client()
+
+# Listen for when a new message is sent
+@bot.event
+async def on_message(msg):
+    # If the message is a command
+    if msg.content == "$test":
+        await msg.channel.send("confirmed")
+        
+# Run the bot
+bot.run(tk) 
