f07a1107b79c5df4741b6e42b886a181db38a1e7
rothman5 <rothman@ualberta.ca>
2022-08-07 00:21:34 -0600

    active server count added

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ tk = os.getenv("TOKEN")
 # Create a client object from the discord.py module
 bot = discord.Client()
 
+# Listen for when the bot switches states
+@bot.event
+async def on_ready():
+    server_count = 0
+
+    for server in bot.guilds:
+        print(f"- {server.id} (name: {server.name})")
+
+        server_count += 1
+
+    print("Derek-Bot is in " + str(server_count) + " guilds.")
+
 # Listen for when a new message is sent
 @bot.event
 async def on_message(msg):
