c66b5c59b833d45897b3b700c72afa9310de4fba
rothman5 <rothman@ualberta.ca>
2022-08-09 00:12:43 -0600

    dot-env module added

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+# Authors: rothman & mike
+import os
 import discord
 import sqlite3
 
+from dotenv import load_dotenv
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
 
+load_dotenv("env/.env")
+
+debug = True
+
 # Create a client object from the discord.py module
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-# Listen for when someone joins the server
-# Create database table if it doesn't exist
-# Adds new users to the database
+# This function listens for server join events
+# and initializes the new user information into
+# a database if the user hasn't joined before. 
 @bot.event
 async def on_member_join(user):    
     # Establish connection to database
@@ -25,26 +32,23 @@ async def on_member_join(user):
     # Select only the user ID column
     select_query = """SELECT id FROM users"""
     cursor.execute(select_query)
-    
+
     ids = {data[0] for data in cursor.fetchall()}
     
     if not user.id in ids:
         cursor.execute("""INSERT INTO users (id, xp) VALUES (?, ?)""", (user.id, 0))
-        print(f"Added {user.id} to the database.")
+        if debug: print(f"[Debug] Added {user.id} to the database.")
     else:
-        print(f"{user.id} is already in the database.")
+        if debug: print(f"[Debug] {user.id} is already in the database.")
     
     # Commit changes and close connection
     connection.commit()
     cursor.close()
     connection.close()
     
-    # Get the welcome channel ID
-    channel = bot.get_channel(1006419916351611051)
-    
-    # Send new member welcome message
+    # Get the welcome channel ID and send a welcome message
+    # channel = bot.get_channel(1006419916351611051)
     # await channel.send(f"{user.mention} has joined the server!")
-    print("User joined")
 
 # Listen for when the bot switches states
 @bot.event
@@ -52,17 +56,16 @@ async def on_ready():
     server_count = 0
 
     for server in bot.guilds:
-        # print(f"- {server.id} (name: {server.name})")
         server_count += 1
 
-    print("Derek-Bot is online in " + str(server_count) + " servers.")
+    print("[Info] Derek-Bot is online. (" + str(server_count) + ")")
 
 # Listen for when a new message is sent
 @bot.event
 async def on_message(msg):
     # If the message is a command
-    if msg.content == "test":
+    if msg.content == "$test":
         await msg.channel.send("confirmed")
-        
+    
 # Run the bot
-bot.run("MTAwNTcxMjQ5MzEwMTk4NTgwMg.GeF0Ad.UIy5V6YRalYqmU1z7K5vw631sELTL7K9dyZKU4") 
+bot.run(os.getenv("TOKEN")) 
